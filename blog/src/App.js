d3498b65dbf4fa73f81c6d2cbd877089d563054c
kristendaya <clare0339@gmail.com>
2023-09-29 05:10:44 +0000

    react

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
 
-  let post = '호주맛집'
+  let post = '호주맛집';
+  let [글제목,b]= useState(['호주날씨','호주여행지','호주favorite']);
+// [호주날씨, 함수] 남음 
+// state쓰는 이유는 재 렌더링
+  
 
   return (
     <div className="App">
       <div className= "black-nav"> 
-        <h4 style={{color:'white', FontSize :'20px'}}> Krisen Daya's blog</h4>
-         </div>
-         <h4>{post}</h4>
+        <h4> Kristendaya's blog</h4>
+      </div>
+      <div className ="list">
+          <h4>{글제목}</h4>
+          <h4>{글제목}</h4>
+          <h4>{글제목}</h4>
+          <p>9월 26일 발행</p>
+      </div>
+      
     </div>
   );
 }
