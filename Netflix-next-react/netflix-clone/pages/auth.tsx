f541fa8feffc2896498d11a01beb87dc43e9e558
Kristen Daya Lee <clare0339@gmail.com>
2023-10-27 20:15:30 +0900

    Netflix

---
+++ Netflix-next-react/netflix-clone/pages/auth.tsx
@@ -0,0 +1,9 @@
+const Auth = () => {
+    return( 
+        // <div style={{ backgroundImage: url(${/images/hero.jpg}) }} 
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        </div>
+        );
+}
+
+export default Auth;
