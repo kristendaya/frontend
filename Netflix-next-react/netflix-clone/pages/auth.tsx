8e9a024cb6848eb6075f31820557e8bd7ad113b5
Kristen Daya Lee <clare0339@gmail.com>
2023-10-28 12:43:47 +0900

    nextJS

--- Netflix-next-react/netflix-clone/pages/auth.tsx
+++ Netflix-next-react/netflix-clone/pages/auth.tsx
@@ -1,7 +1,25 @@
+import Input from "../components/Input";
+
 const Auth = () => {
     return( 
         // <div style={{ backgroundImage: url(${/images/hero.jpg}) }} 
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            {/* bg-cover : 꽉차게  */}
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="Logo" className="h-12"/>
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            Sign in
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            <Input />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         );
 }
