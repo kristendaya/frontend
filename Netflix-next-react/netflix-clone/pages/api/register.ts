f616dc00beaf1f5a75e2a554f9d013ba7b10d7c9
Kristen Daya Lee <clare0339@gmail.com>
2023-11-24 18:25:36 +0900

    react-nextJS

---
+++ Netflix-next-react/netflix-clone/pages/api/register.ts
@@ -0,0 +1,29 @@
+import bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse} from 'next';
+import prismadb from '@/lib/prismadb';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+   if (req.method != 'POST'){
+      return res.status(405).end();
+   }
+
+   try {
+      const { email, name, password } = req.body;
+
+      const existingUser = await prismadb.user.findUnique({
+         where:{
+            email,
+         }
+      });
+
+      if (existingUser){
+         return res.status(422).json({error: 'EMail taken'});
+      }
+ 
+      const hashedPasswrd = await bcrypt.hash(password,12);
+
+   }catch(error){
+      console.log(error);
+      return res.status(400).end();
+   }
+}
