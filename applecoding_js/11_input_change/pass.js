1291fe6c06dc3d8e50f175c30b2d9db9fd9a3cfe
Kristen Daya Lee <clare0339@gmail.com>
2024-01-30 16:23:04 +0900

    pr

---
+++ applecoding_js/11_input_change/pass.js
@@ -0,0 +1,11 @@
+
+
+function passscore (sc1,sc2 ) {
+  if (sc1+sc2 >= 120 && sc1> 40 && sc2 >40){
+   console.log("합격")
+  }
+  else {console.log("불합격")}   
+};
+
+passscore(70,70);
+passscore(30,100);
---
+++ applecoding_js/11_input_change/threegame.js
@@ -0,0 +1,14 @@
+// function three(num){
+//    if (num % 9 == 0) {
+//       console.log("박수*2");
+//    } else if (num%3 == 0 ) {
+//       console.log("박수")
+//    } else {
+//       console.log('pass')
+//    }
+// }
+
+// three(20);
+// three(18);
+// three(9);
+// three(3);
