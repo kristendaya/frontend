c6ff6f4b0e13e8736c99b42130b965fffdd558c5
Kristen Daya Lee <clare0339@gmail.com>
2024-03-16 01:00:54 +0900

    newsletter

--- applecoding_js/11_input_change/pass.js
+++ applecoding_js/11_input_change/pass.js
@@ -4,8 +4,9 @@ function passscore (sc1,sc2 ) {
   if (sc1+sc2 >= 120 && sc1> 40 && sc2 >40){
    console.log("합격")
   }
-  else {console.log("불합격")}   
-};
+  else {console.log("불합격")   
+   }
+}
 
 passscore(70,70);
 passscore(30,100);
--- applecoding_js/11_input_change/threegame.js
+++ applecoding_js/11_input_change/threegame.js
@@ -12,3 +12,35 @@
 // three(18);
 // three(9);
 // three(3);
+
+//var num =12345;
+// function samyukku (numbers){
+//    const num = numbers.toString();
+//    if (num.length-1 ===3 || num.length-1==='6' || num.length-1==='9'){
+
+//    console.log('clap');
+// } 
+//      else {
+//    console.log('pass')
+//    }
+// }
+
+// samyukku(9);
+// samyukku(12);
+// samyukku(33);
+// samyukku(23);
+
+function samyukku (num){
+   const strNum = num.toString()
+   const lastNum = strNum.charAt(strNum.length-1);
+   if(lastNum == 3 || lastNum == 6 || lastNum==9){
+      console.log("박수")
+   } else {console.log("pass")
+   }
+}
+
+
+samyukku(23)
+samyukku(35)
+samyukku(43)
+samyukku(12)
