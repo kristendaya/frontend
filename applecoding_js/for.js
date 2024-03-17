c76bbacd7965fe420de9fe3c857d9e82e9855b47
Kristen Daya Lee <clare0339@gmail.com>
2024-03-18 00:34:02 +0900

    js

---
+++ applecoding_js/for.js
@@ -0,0 +1,17 @@
+for (let i=1; i<=100; i++){
+   if(i%2 ==0){
+      console.log(i);
+   }
+}
+
+//
+
+for (let i; i<=50; i++){
+      console.log(i*2);
+   }
+
+   //ㅑfmf 2씩 증가시키는 방법
+
+for (let i=2; i<=100; i+=2){
+   console.log(i);
+}
